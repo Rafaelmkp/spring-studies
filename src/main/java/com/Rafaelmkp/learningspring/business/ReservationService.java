17f3ca9c89430808971201b09bacfee79e3d3349
Rafaelmkp <rafaelmkp@hotmail.com>
2022-08-02 09:07:03 -0300

    fix dependency injection in reservation service

--- src/main/java/com/Rafaelmkp/learningspring/business/ReservationService.java
+++ src/main/java/com/Rafaelmkp/learningspring/business/ReservationService.java
@@ -7,9 +7,19 @@ import java.util.*;
 
 @Service
 public class ReservationService {
-    private RoomRepository roomRepository;
-    private GuestRepository guestRepository;
-    private ReservationRepository reservationRepository;
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(
+            RoomRepository roomRepository,
+            GuestRepository guestRepository,
+            ReservationRepository reservationRepository
+    ) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
 
     public List<RoomReservation> getRoomReservationsForDate(Date date) {
         Iterable<Room> rooms = roomRepository.findAll();
