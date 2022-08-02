413d7c2252d98f56d848373071e8fb750f03ee59
Rafaelmkp <rafaelmkp@hotmail.com>
2022-08-02 08:57:10 -0300

    add reservation service and dto

---
+++ src/main/java/com/Rafaelmkp/learningspring/business/ReservationService.java
@@ -0,0 +1,54 @@
+package com.Rafaelmkp.learningspring.business;
+
+import com.Rafaelmkp.learningspring.data.*;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ReservationService {
+    private RoomRepository roomRepository;
+    private GuestRepository guestRepository;
+    private ReservationRepository reservationRepository;
+
+    public List<RoomReservation> getRoomReservationsForDate(Date date) {
+        Iterable<Room> rooms = roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getRoomNumber());
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+
+        Iterable<Reservation> reservations =
+                reservationRepository.findReservationByDate(new java.sql.Date(date.getTime()));
+        reservations.forEach(reservation -> {
+            RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
+            roomReservation.setDate(date);
+            Guest guest = guestRepository.findById(reservation.getGuestId()).get();
+            roomReservation.setFirstName(guest.getFirstName());
+            roomReservation.setLastName((guest.getLastName()));
+            roomReservation.setGuestId(guest.getId());
+        });
+
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        for(Long id : roomReservationMap.keySet()) {
+            roomReservations.add(roomReservationMap.get(id));
+        }
+
+        roomReservations.sort((o1, o2) -> {
+            if(o1.getRoomName().equals(o2.getRoomName())) {
+                return o1.getRoomNumber().compareTo(o2.getRoomNumber());
+            }
+            return o1.getRoomName().compareTo(o2.getRoomName());
+        });
+
+        return roomReservations;
+    }
+
+
+
+
+}
