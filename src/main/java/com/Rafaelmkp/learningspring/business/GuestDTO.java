9a6e62002bd27daa91ff649e78d51fff828cd8e7
Rafaelmkp <rafaelmkp@hotmail.com>
2022-08-05 08:42:48 -0300

    create dto for guest

---
+++ src/main/java/com/Rafaelmkp/learningspring/business/GuestDTO.java
@@ -0,0 +1,50 @@
+package com.Rafaelmkp.learningspring.business;
+
+public class GuestDTO {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "GuestDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
